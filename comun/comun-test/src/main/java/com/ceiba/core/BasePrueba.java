90af8f9a9bbfd9b5e3fcfef55cf8683d713241c8
JuanHurtado95 <juan.carlos.hurtado@correounivalle.edu.co>
2021-09-29 09:24:58 -0500

    commit version Jenkins

--- comun/comun-test/src/main/java/com/ceiba/core/BasePrueba.java
+++ comun/comun-test/src/main/java/com/ceiba/core/BasePrueba.java
@@ -1,9 +1,10 @@
 package com.ceiba.core;
 
+import org.junit.Assert;
+
 import java.util.function.Supplier;
 
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.fail;
 
 
 public class BasePrueba {
@@ -16,9 +17,9 @@ public class BasePrueba {
 			supplier.get();
 			fail();
 		} catch (Exception e) {
-			assertTrue(exception.isInstance(e), SE_ESPERABA_LA_EXCEPCION + exception.getCanonicalName() + PERO_FUE_LANZADA
-					+ e.getClass().getCanonicalName());
-			assertTrue(e.getMessage().contains(message));
+			Assert.assertTrue(SE_ESPERABA_LA_EXCEPCION + exception.getCanonicalName() + PERO_FUE_LANZADA
+								+ e.getClass().getCanonicalName(), exception.isInstance(e));
+			Assert.assertTrue(e.getMessage().contains(message));
 		}
 	}
 
@@ -27,9 +28,9 @@ public class BasePrueba {
 			thunk.execute();
 			fail();
 		} catch (Exception e) {
-			assertTrue(exception.isInstance(e), SE_ESPERABA_LA_EXCEPCION + exception.getCanonicalName() + PERO_FUE_LANZADA
-					+ e.getClass().getCanonicalName());
-			assertTrue(e.getMessage().contains(message));
+			Assert.assertTrue(SE_ESPERABA_LA_EXCEPCION + exception.getCanonicalName() + PERO_FUE_LANZADA
+								+ e.getClass().getCanonicalName(), exception.isInstance(e));
+			Assert.assertTrue(e.getMessage().contains(message));
 		}
 	}
 
